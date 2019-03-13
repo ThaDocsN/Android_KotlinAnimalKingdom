d83b5f4890db0f866e85f6a3e3799b29456dd96d
ThaDocIzN <rodan72@msn.com>
2019-03-13 04:23:28 -0500

    added collections

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    var animalsList = mutableListOf(
+        Mammal("Panda", 1869),
+        Mammal("Zebra", 1778),
+        Mammal("Koala", 1816),
+        Mammal("Sloth", 1804),
+        Mammal("Armadillo", 1758),
+        Mammal("Raccoon", 1758),
+        Mammal("Bigfoot", 2021),
+
+        Birds("Pigeon", 1837),
+        Birds("Peacock", 1821),
+        Birds("Toucan", 1758),
+        Birds("Parrot", 1824),
+        Birds("Swan", 1758),
+
+        Fish("Salmon", 1758),
+        Fish("Catfish", 1817),
+        Fish("Perch", 1758)
+    )
+}
